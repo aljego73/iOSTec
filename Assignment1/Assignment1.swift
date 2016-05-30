fc1d47469c4d7e62758c5a15c952be7711b987fe
Alejandro Jesús <alejandro.jesus@gmail.com>
2016-05-29 22:47:50 -0500

    Primer Commit

---
+++ Assignment1/Assignment1.swift
@@ -0,0 +1,12 @@
+//
+//  Assignment1.swift
+//  Assignment1
+//
+//  Created by Alejandro Jesus on 29/05/16.
+//  Copyright © 2016 Alejandro Jesus. All rights reserved.
+//
+
+
+// Pruebas
+
+import Foundation
