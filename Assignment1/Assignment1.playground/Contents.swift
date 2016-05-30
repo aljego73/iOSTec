edddad0743c4a38a5895a0d22f10bc44943ae5aa
Alejandro Jesús <alejandro.jesus@gmail.com>
2016-05-29 23:12:09 -0500

    Creo que ésta es la buena. Ya pude incluir el playground dentro del proyecto y subirlo a Gist

---
+++ Assignment1/Assignment1.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Swift: programar para iOS - Coursera
+//: Ejercicio d final de semana 2
+//: Alejandro Jesús González 😅
+
+import Cocoa
+
+var cont = 0...100
+
+for i in cont {
+    
+    if (i != 0) && (i % 5 == 0) {
+        print("\(i)\tBingo")
+    }
+    
+    if (i % 2 == 0) {
+        print("\(i)\tPar")
+    }
+        
+    else {
+        print("\(i)\tImpar")
+    }
+    
+    if (i>=30) && (i<=40) {
+        print("\(i)\tviva Swift!!!")
+    }
+}
